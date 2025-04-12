21e29b4580d9efcbe718f2e0c477b4675adced08
Omkar Prajapati <prajapatiomkar9833@gmail.com>
2025-04-12 11:31:44 +0530

    fix the register code flow.

--- src/controllers/register.controller.ts
+++ src/controllers/register.controller.ts
@@ -3,22 +3,43 @@ import { CODES } from "../constants/status.code";
 import bcrypt from "bcryptjs";
 import { User } from "../models/user.model";
 
+interface RegisterRequestBody {
+  email: string;
+  username: string;
+  password: string;
+}
 const registerController = async (
-  req: Request,
+  req: Request<{}, {}, RegisterRequestBody>,
   res: Response,
   next: NextFunction
 ) => {
   try {
     const { email, password, username } = req.body;
 
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res.status(CODES.CONFLICT).json({
+        message: "User already exists with this email.",
+      });
+      return;
+    }
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.insertOne({
-      username,
+
+    const createUser = await User.insertOne({
       email,
+      username,
       password: hashedPassword,
     });
-    console.log(newUser);
-    res.status(CODES.OK).json({ message: "register success!" });
+
+    console.log(createUser);
+    res.status(CODES.CREATED).json({
+      message: "User registered successfully.",
+      user: {
+        _id: createUser._id,
+        email,
+        username,
+      },
+    });
   } catch (error) {
     next(error);
   }
